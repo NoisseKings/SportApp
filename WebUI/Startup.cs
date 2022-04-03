546575ddd1f31b3dc43109df3c2afa4bcdfbc2bf
NOISSE <erpnoisse369@gmail.com>
2022-04-04 01:00:41 +0200

    fixing bugs

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -129,11 +129,11 @@ namespace WebUI
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
+                spa.UseAngularCliServer(npmScript: "start");
+                // if (env.IsDevelopment())
+                // {
+                //     spa.UseAngularCliServer(npmScript: "start");
+                // }
             });
         }
     }
